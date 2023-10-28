151256e57241532a26bf4a8c14268428b7fef079
Sergey Andronnikov <sergey.andronnikov@altexsoft.com>
2023-10-28 20:47:28 +0300

    Brightness

--- src/settings.h
+++ src/settings.h
@@ -71,7 +71,7 @@ typedef struct DayForecast {
 } DayForecast;
 
 RectangleDef timeSpritePos = {0, 0, 320, 88};
-RectangleDef lightSpritePos = {20, 95, 70, 20};
+RectangleDef lightSpritePos = {10, 95, 70, 20};
 
 const String WIND_ICON_NAMES[] = {"N", "NE", "E", "SE", "S", "SW", "W", "NW"};
 
